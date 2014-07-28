0db7a2c49e739352ae3d06f647137fa092c673fa
Dror Poliak <drorp24@gmail.com>
2014-07-28 12:23:28 +0300

    assets precompile

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -28,6 +28,7 @@ class VersionsController < ApplicationController
     else
       @version = @campaign.versions.build
     end
+    @version.locale_id = 1 unless @version.local_id
     @version.save
     session[:version_id] = @version.id
     @customer = @customer_id = session[:customer_id] = nil
