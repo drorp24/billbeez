dad40dda8a5189dd61e9ae5e4a1d414ea01f161d
Dror Poliak <drorp24@gmail.com>
2014-09-13 21:50:47 +0300

    .

--- app/controllers/user_mailer_controller.rb
+++ app/controllers/user_mailer_controller.rb
@@ -1,11 +1,11 @@
 class UserMailerController < ApplicationController
 
-  def weekly()   #ToDo: don't pass version_id parameter
+  def weekly()   
     @newsletter = Newsletter.find(params[:newsletter_id]) 
     @version = @newsletter.version 
     @section = params[:section]
     @control = params[:control]
-    render layout: !request.xhr?
+   render layout: !request.xhr?
   end
   
 end
